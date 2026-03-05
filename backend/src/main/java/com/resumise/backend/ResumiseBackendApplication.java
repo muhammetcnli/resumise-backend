61a648c2730af9ea89d2ed635848ef5d6614b06c
Muhammet Canlı <howardroark756@gmail.com>
2026-03-05 22:53:00 +0300

    Initial commit

---
+++ backend/src/main/java/com/resumise/backend/ResumiseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resumise.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumiseBackendApplication.class, args);
+	}
+
+}
