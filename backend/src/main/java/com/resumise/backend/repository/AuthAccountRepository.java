ccddb5ea11f5c54ad2903b864fbdb25bcbcbc293
Muhammet Canlı <howardroark756@gmail.com>
2026-04-15 16:19:56 +0300

    Create AuthAccountRepository.java

---
+++ backend/src/main/java/com/resumise/backend/repository/AuthAccountRepository.java
@@ -0,0 +1,13 @@
+package com.resumise.backend.repository;
+
+import com.resumise.backend.model.AuthAccount;
+import com.resumise.backend.model.AuthProvider;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthAccountRepository extends JpaRepository<AuthAccount, Long> {
+    Optional<AuthAccount> findByProviderAndProviderUserId(AuthProvider provider, String providerUserId);
+}
