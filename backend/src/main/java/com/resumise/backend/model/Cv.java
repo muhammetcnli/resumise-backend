a4e84bcdbe8befd4c722099ff0b310fd32f8c92d
Muhammet Canlı <howardroark756@gmail.com>
2026-04-17 23:50:28 +0300

    Update Cv.java

--- backend/src/main/java/com/resumise/backend/model/Cv.java
+++ backend/src/main/java/com/resumise/backend/model/Cv.java
@@ -10,9 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +20,8 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cv {
 
     @Id
