2255af84330230667cc6d3a0cf3d7270f1bf5521
Muhammet Canlı <howardroark756@gmail.com>
2026-04-20 23:49:55 +0300

    Create CredentialRepository.java

---
+++ backend/src/main/java/com/resumise/backend/repository/CredentialRepository.java
@@ -0,0 +1,14 @@
+package com.resumise.backend.repository;
+
+import com.resumise.backend.model.Credential;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CredentialRepository extends JpaRepository<Credential, Long> {
+
+    Optional<Credential> findByUserId(Long userId);
+}
+
